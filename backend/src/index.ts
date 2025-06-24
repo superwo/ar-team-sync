a8a0be05b2df17ac60d87aff0f0d0502c5301f19
Radu <radu.avadanii@gmail.com>
2025-06-24 13:18:29 +0300

    upd: backend error handling.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,12 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -12,32 +18,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-  session({
-    name: "session",
-    keys: [config.SESSION_SECRET],
-    maxAge: 24 * 60 * 60 * 1000,
-    secure: config.NODE_ENV === "production",
-    httpOnly: true,
-    sameSite: "lax",
-  })
+    session({
+        name: "session",
+        keys: [config.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: config.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax",
+    })
 );
 
 app.use(
-  cors({
-    origin: config.FRONTEND_ORIGIN,
-    credentials: true,
-  })
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true,
+    })
 );
 
 app.get(
-  `/`,
-   (req: Request, res: Response, next: NextFunction) => {
-        res.status(200).json({
-            message: "Hello World!",
-        })
-  })
+    `/`,
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        throw new BadRequestException(
+            "This is a bad request",
+            ErrorCodeEnum.AUTH_INVALID_TOKEN
+        );
+        return res.status(HTTPSTATUS.OK).json({
+            message: "Hello User 👋",
+        });
+    })
+);
 
+app.use(errorHandler);
 
-app.listen(config.PORT, () => {
-  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+app.listen(config.PORT, async () => {
+    console.log(
+        `Server listening on port ${config.PORT} in ${config.NODE_ENV}`
+    );
+    await connectDatabase();
 });
