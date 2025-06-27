d8211f716a8167de8f12fcc997486c59469ad442
Radu <radu.avadanii@gmail.com>
2025-06-27 16:18:46 +0300

    upd: index routes.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,24 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
-import passport from "passport";
 import session from "cookie-session";
 import { config } from "./config/app.config";
 import connectDatabase from "./config/database.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
-import { BadRequestException } from "./utils/appError";
-import { ErrorCodeEnum } from "./enums/error-code.enum";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
-import authRoutes from "./routes/auth.route";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
 
 import "./config/passport.config";
+import passport from "passport";
+import authRoutes from "./routes/auth.route";
+// import userRoutes from "./routes/user.route";
+// import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+// import workspaceRoutes from "./routes/workspace.route";
+// import memberRoutes from "./routes/member.route";
+// import projectRoutes from "./routes/project.route";
+// import taskRoutes from "./routes/task.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -50,12 +56,17 @@ app.get(
             ErrorCodeEnum.AUTH_INVALID_TOKEN
         );
         return res.status(HTTPSTATUS.OK).json({
-            message: "Hello User 👋",
+            message: "Hello Subscribe to the channel & share",
         });
     })
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes);
+// app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+// app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+// app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+// app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+// app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 
 app.use(errorHandler);
 
